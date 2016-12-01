530b533ff209a595e63ef49f851769da19ff35c3
Thomas Marren <tmarren2090@gmail.com>
2016-12-01 13:46:08 -0500

    integrate iframe and postlight api

--- your-agora-fe/src/actions/actions.js
+++ your-agora-fe/src/actions/actions.js
@@ -11,3 +11,15 @@ export function fetchUsers(searchTerm){
     })
   }
 }
+
+export function getArticle(searchTerm){
+ return function(dispatch){
+   $.ajax({url: `https://mercury.postlight.com/parser?url=${searchTerm}`,
+           type: "GET",
+           contentType: "application/json",
+           headers: {"x-api-key": "T9b6wK7r9S9HC6jFHYtDY9EaErommV6q3wu0bRcc"}})
+     .done(function(data){
+     dispatch({type: 'FETCH_CONTENT', payload: data})
+     })
+    }
+}

1a980b8ba0a1a3005c5cdb7578de7de73fb96b26
wcpines <colby@aptible.com>
2016-12-01 12:29:51 -0500

    Auth is working

--- your-agora-fe/src/actions/actions.js
+++ your-agora-fe/src/actions/actions.js
@@ -2,13 +2,11 @@ import $ from 'jquery'
 
 export function fetchUsers(searchTerm){
   return function(dispatch){
-    $.ajax({url: `http://localhost:3000/users/${searchTerm}`,
-            type: "GET"
-          })
-      .done(function(data){
-
-        debugger
-
+    $.ajax({url: `http://localhost:3000/users`,
+      type: "GET",
+      headers: {authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.KKOVi8tFRFO8zz6Jg9se4f14XfknuWcgtDArATr2PE4"}
+    }).done(function(data){
+      debugger
       dispatch({type: 'FETCH_USERS', payload: data})
     })
   }

b541b9969d5af01869c793f20e25a7baea8f61a1
wcpines <colby@aptible.com>
2016-12-01 12:29:51 -0500

    Auth is working

--- your-agora-api/app/controllers/users_controller.rb
+++ your-agora-api/app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
   def index
+    binding.pry
     @users = User.all
     render json: @users
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end

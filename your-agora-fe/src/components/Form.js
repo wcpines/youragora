09d211b0f292d2702bb388b60afa9d64a80d5b01
wcpines <colby@aptible.com>
2016-12-01 11:27:02 -0500

    Let there be boilerplate!

---
+++ your-agora-fe/src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { fetchUsers } from '../actions/actions';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+function Form(props){
+
+  function handleSubmit(event){
+    event.preventDefault()
+    props.fetchUsers(event.currentTarget.children[0].value)
+  }
+
+  return (
+    <form onSubmit={handleSubmit.bind(props)}>
+      <input type="text" />
+      <input type="submit" value="Dino Danger!" />
+    </form>
+  )
+
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchUsers: fetchUsers }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(Form)

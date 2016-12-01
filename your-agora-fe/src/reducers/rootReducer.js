7dec3e4c5f9ba82aabcc8f9c2b76d8b18b8920a0
wcpines <colby@aptible.com>
2016-12-01 11:27:02 -0500

    Let there be boilerplate!

---
+++ your-agora-fe/src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+
+// function users(state = [], action){
+//   switch (action.type) {
+//     case "FETCH_VIDEOS":
+//       return action.payload.videos
+//     default:
+//       return state
+//   }
+// }
+
+const rootReducer = combineReducers({})
+
+export default rootReducer

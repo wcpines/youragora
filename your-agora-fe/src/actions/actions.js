a8dbc6cffc2ef327c85b8e6ec6b1f7dfe2a29999
Thomas Marren <tmarren2090@gmail.com>
2016-12-02 10:03:11 -0500

    update getArticle to update state with all content we want

--- your-agora-fe/src/actions/actions.js
+++ your-agora-fe/src/actions/actions.js
@@ -13,13 +13,21 @@ export function fetchUsers(searchTerm){
 }
 
 export function getArticle(searchTerm){
- return function(dispatch){
-   $.ajax({url: `https://mercury.postlight.com/parser?url=${searchTerm}`,
-           type: "GET",
-           contentType: "application/json",
-           headers: {"x-api-key": "T9b6wK7r9S9HC6jFHYtDY9EaErommV6q3wu0bRcc"}})
-     .done(function(data){
-     dispatch({type: 'FETCH_CONTENT', payload: data})
-     })
+  return function(dispatch){
+    $.ajax({url: `https://mercury.postlight.com/parser?url=${searchTerm}`,
+      type: "GET",
+      contentType: "application/json",
+      headers: {"x-api-key": "T9b6wK7r9S9HC6jFHYtDY9EaErommV6q3wu0bRcc"}})
+      .done(function(data){
+        let article = {
+          title: data.title,
+          author: data.author,
+          image_url: data.lead_image_url,
+          url: data.url,
+          content: data.content,
+          date_published: data.date_published
+        }
+        dispatch({type: 'FETCH_ARTICLE', payload: article})
+      })
     }
-}
+  }

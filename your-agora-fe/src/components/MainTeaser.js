bc1db5d2fc7ab964a3fa92c79e20885eb2b26733
wcpines <colby@aptible.com>
2016-12-02 20:54:25 -0500

    Add some labels and fix indent

--- your-agora-fe/src/components/MainTeaser.js
+++ your-agora-fe/src/components/MainTeaser.js
@@ -4,13 +4,13 @@ import {Link} from 'react-router';
 
 
 function MainTeaser(props){
-	return(
-		<div >
-			<img className="thumbnail" src={props.image_url} />
-      		<h1>{props.title}</h1>
-    		<Link to="mainarticle" className="button" >Read More</Link>
-    	</div>
-		)
+  return(
+    <div >
+      <img className="thumbnail" src={props.image_url} />
+      <h1>{props.title}</h1>
+      <Link to="mainarticle" className="button" >Read More</Link>
+    </div>
+  )
 }
 
 
--- your-agora-fe/src/components/SearchBar.js
+++ your-agora-fe/src/components/SearchBar.js
@@ -5,31 +5,37 @@ import { bindActionCreators } from 'redux'
 import {Link} from 'react-router';
 
 class SearchBar extends Component{
-    handleClick(event){
-        event.preventDefault()
-        this.props.getArticle(event.target.children[0].value)
-    }
+  handleClick(event){
+    event.preventDefault()
+    this.props.getArticle(event.target.children[0].value)
+  }
 
-    render(){
-        return(<div >
-            <form className='search-bar' onSubmit={this.handleClick.bind(this)}>
-                <input type="text" />
-                <label className='search-bar'>Search Bar</label>
-                <input type='submit' className='search-bar' name='submit' />
-            </form>
-                        <Link to="mainarticle" className="button" >see article</Link>
+  render(){
+    return(
 
-            </div>
-            )
-    }
+      <div >
+
+        <form className='search-bar' onSubmit={this.handleClick.bind(this)}>
+
+          <label htmlFor='search' className='search-bar'>Search for a topic: </label>
+          <input id='search' type="text" />
+          <input type='submit' className='search-bar' name='submit' value='Search'/>
+
+        </form>
+
+        <Link to="mainarticle" className="button" >see article</Link>
+
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state){
-    return state
+  return state
 }
 
 function mapDispatchToProps(dispatch){
-    return bindActionCreators({ getArticle }, dispatch)
+  return bindActionCreators({ getArticle }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchBar)

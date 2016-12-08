44a08285329c3af005077168ed76866ccb76fd45
mkevinevans <mkevinevans@gmail.com>
2016-12-07 20:48:46 -0500

    make homepage component smart in order to user fetchUserId on mount to show user name on refresh

--- your-agora-fe/src/components/HomePage.js
+++ your-agora-fe/src/components/HomePage.js
@@ -1,39 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
 import SearchBar from './articles/SearchBar'
 import SignOut from './users/SignOut'
+import fetchUserId from '../actions/fetchUserId'
+import { bindActionCreators } from 'redux'
 
-function HomePage(props) {
 
-  var loggedIn;
-
-  if (localStorage.jwt === undefined){
-    loggedIn =
-    <div className="sign-links-homepage">
-      <Link id="sign-in-link" to="/signin" >Sign In</Link>
-      <strong>|</strong>
-      <Link id="sign-up-link" to="/signup" >Sign Up</Link>
-    </div>
-  } else {
-    loggedIn = <div className="sign-links"> <span id="welcome-message"> Welcome back {props.userName}! </span> <br /> <SignOut /> </div>
+class HomePage extends Component{
+  constructor(props) {
+    super(props)
   }
 
-  return (
-    <div className="HomePage">
-      {loggedIn}
-      <div id="homepage-logo">
-        <img role="presentation" src="/images/youragora-logo.png" />
+  componentWillMount(){
+    if(this.props.id === undefined && localStorage.getItem('jwt') != null){
+      this.props.fetchUserId()
+    }
+  }
+
+  render(){
+    debugger
+    var loggedIn;
+  
+    if (localStorage.jwt === undefined){
+      loggedIn =
+      <div className="sign-links-homepage">
+        <Link id="sign-in-link" to="/signin" >Sign In</Link>
+        <strong>|</strong>
+        <Link id="sign-up-link" to="/signup" >Sign Up</Link>
       </div>
-      <div id="search-bar-homepage-container">
-        <SearchBar idProps={'search-bar-homepage'}/>
-      </div>
-    </div>
-  )
+    } else {
+      loggedIn = <div className="sign-links"> <span id="welcome-message"> Welcome back {this.props.userName}! </span> <br /> <SignOut /> </div>
+    }
+      return (
+        <div className="HomePage">
+          {loggedIn}
+          <div id="homepage-logo">
+            <img role="presentation" src="/images/youragora-logo.png" />
+          </div>
+          <div id="search-bar-homepage-container">
+            <SearchBar idProps={'search-bar-homepage'}/>
+          </div>
+        </div>
+      )
+  }
 }
 
 function mapStateToProps(state){
-  return {userName: state.currentUser.userName}
+  return {userName: state.currentUser.userName,
+          id: state.currentUser.UserId}
 }
 
-export default connect(mapStateToProps)(HomePage)
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchUserId }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)

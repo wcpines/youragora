88e92d86193819e6fc783a4733748728cfae47bb
Thomas Marren <tmarren2090@gmail.com>
2016-12-10 10:33:04 -0500

    fix article controller to send back articles formatted correctly

--- your-agora-api/app/controllers/articles_controller.rb
+++ your-agora-api/app/controllers/articles_controller.rb
@@ -4,54 +4,55 @@ class ArticlesController < ApplicationController
 
   def fetch_first_article
 
-    unless params[:current_user_id].nil? 
-
-      @user = User.find params[:current_user_id]
-
-      sources = WeightedSourceGenerator.new(@user).get_weighted_sources
-      # => [ full of source objects ]
-      source_domains = sources.reduce(Hash.new(0)) {|source_domain, quantity| source_domain[quantity] += 1; source_domain}
-      # => {Mises: 2, Huff: 3, ...}
-    else
-
-      sources = RandomSourceGenerator.random_sources
-
-      source_domains = sources.reduce(Hash.new(0)) {|source_domain, quantity| source_domain[quantity] += 1; source_domain}
-
-    end
-
-    articles = []
-
-    source_domains.each do |domain, num_of_articles|
-      case domain
-      when "mises.org"
-        Mises.get_articles(num_of_articles, params[:search_term]).each do |article_url|
-          articles << {url: article_url, source_id: 6}
-        end
-      else
-        GoogleNews.get_articles(num_of_articles, params[:search_term], domain).each do |article_url|
-          articles << {url: article_url, source_id: Source.find_by(domain: domain).id}
-        end
-      end
-    end
-
-    @full_articles = articles.map do |url_and_source|
-      article = Article.find_by(url: url_and_source[:url])
-      if article.nil?
-        parser = ArticleParser.get_article_html(url_and_source[:url])
-        next if parser == "Article not found"
-        parser.assign_attributes(url_and_source)
-        parser.save
-        article = parser
-      end
-
-      {article: article, source_name: Source.find(url_and_source[:source_id]).name}
-
-    end
-
-    # @full_articles = Article.order("RANDOM()")[0...2].map do |article|
-    #   {article: article, source_name: Source.find(article.source_id).name}
+    # unless params[:current_user_id].nil?
+    #
+    #   @user = User.find params[:current_user_id]
+    #
+    #   sources = WeightedSourceGenerator.new(@user).get_weighted_sources
+    #   # => [ full of source objects ]
+    #   source_domains = sources.reduce(Hash.new(0)) {|source_domain, quantity| source_domain[quantity] += 1; source_domain}
+    #   # => {Mises: 2, Huff: 3, ...}
+    # else
+    #
+    #   sources = RandomSourceGenerator.random_sources
+    #
+    #   source_domains = sources.reduce(Hash.new(0)) {|source_domain, quantity| source_domain[quantity] += 1; source_domain}
+    #
     # end
+    #
+    # articles = []
+    #
+    # source_domains.each do |domain, num_of_articles|
+    #   case domain
+    #   when "mises.org"
+    #     Mises.get_articles(num_of_articles, params[:search_term]).each do |article_url|
+    #       articles << {url: article_url, source_id: 6}
+    #     end
+    #   else
+    #     GoogleNews.get_articles(num_of_articles, params[:search_term], domain).each do |article_url|
+    #       articles << {url: article_url, source_id: Source.find_by(domain: domain).id}
+    #     end
+    #   end
+    # end
+    #
+    # @full_articles = articles.map do |url_and_source|
+    #   article = Article.find_by(url: url_and_source[:url])
+    #   if article.nil?
+    #     parser = ArticleParser.get_article_html(url_and_source[:url])
+    #     next if parser == "Article not found"
+    #     parser.assign_attributes(url_and_source)
+    #     parser.save
+    #     article = parser
+    #   end
+    #
+    #   article.attributes.merge({"sourceName" => article.source.name})
+    #
+    # end
+
+    @full_articles = Article.order("RANDOM()")[0...2].map do |article|
+      article.attributes.merge({"sourceName" => article.source.name})
+      # !!Note Source Name camel case because it's going to JS
+    end
 
     render json: @full_articles
 
@@ -103,13 +104,14 @@ class ArticlesController < ApplicationController
     #       article = parser
     #     end
     #
-    #     {article: article, source_name: Source.find(url_and_source[:source_id]).name}
+    #     article.attributes.merge({"sourceName" => article.source.name})
     #
     #   end
     #
 
     @full_articles = Article.order("RANDOM()")[0...2].map do |article|
-      {article: article, source_name: Source.find(article.source_id).name}
+      article.attributes.merge({"sourceName" => article.source.name})
+      # !!Note Source Name camel case because it's going to JS
     end
 
     render json: @full_articles

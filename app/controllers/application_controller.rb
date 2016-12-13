0b44b061585c478ff34a31e550c146238caf2b7e
mkevinevans <mkevinevans@gmail.com>
2016-12-13 10:25:56 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,10 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
-    # NOTE: This is fixing something that's probably due to us implementing this incorrectly.
-    # Why does request method change request.env object type to/from an array?
 
     unless request.env["HTTP_AUTHORIZATION"].empty?
 
-      # if request.env["REQUEST_METHOD"] == "POST"
-      #   auth_string = Auth.decode(request.env["HTTP_AUTHORIZATION"])[0] # dafuq
-      # else
-      #   auth_string = Auth.decode(request.env["HTTP_AUTHORIZATION"])
-      # end
-
-      auth_string = Auth.decode(request.env["HTTP_AUTHORIZATION"])[0] # 
+      auth_string = Auth.decode(request.env["HTTP_AUTHORIZATION"])[0] #
 
       @user = User.find(auth_string["user_id"])
 

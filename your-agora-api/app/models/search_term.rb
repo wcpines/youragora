31b3edf1bc916bf173320d50615ceb9cf1862b7c
wcpines <colby@aptible.com>
2016-12-13 16:29:18 -0500

    Analytics slickness

--- your-agora-api/app/models/search_term.rb
+++ your-agora-api/app/models/search_term.rb
@@ -2,10 +2,11 @@
 #
 # Table name: search_terms
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  name         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  search_count :integer
 #
 
 class SearchTerm < ApplicationRecord
--- your-agora-api/db/schema.rb
+++ your-agora-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213151959) do
+ActiveRecord::Schema.define(version: 20161213212827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,9 @@ ActiveRecord::Schema.define(version: 20161213151959) do
 
   create_table "search_terms", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "search_count"
   end
 
   create_table "sources", force: :cascade do |t|

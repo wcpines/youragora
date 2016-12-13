67f483e0f5b483c4627d7e48c73f92d2edf53e1c
wcpines <colby@aptible.com>
2016-12-13 10:21:40 -0500

    Add search terms to articles

---
+++ your-agora-api/app/models/search_term.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: search_terms
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class SearchTerm < ApplicationRecord
+end

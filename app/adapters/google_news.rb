c32f1541d9ea7a081212dfb621aaca078a73d376
mkevinevans <mkevinevans@gmail.com>
2016-12-16 17:01:31 -0500

    print articles

--- app/adapters/google_news.rb
+++ app/adapters/google_news.rb
@@ -19,13 +19,15 @@ class GoogleNews
       formatted_links = correct_publications.map { |publication| publication.css('.r').children.map(&:attributes)[0]["href"].value.split(/(http.*)/)[1].split('&')[0]} # get just the URI
       formatted_links[0...num_of_articles].map { |link| URI.decode(link) }  # handle cases where URIs are returned encoded.
 
-    elsif call_api
+    elsif call_api(seach_term, num_of_articles, domain)
 
       formatted_search = search_term.split(" ").join("%20")
       results = JSON.parse(open("https://www.googleapis.com/customsearch/v1?q=#{formatted_search}&cx=007438961960256472316%3A4gdkpqmpbru&num=#{num_of_articles}&siteSearch=#{domain}&sort=date&key=#{KEY}").read)
 
       results['items'].map do |result|
         result['link']
+      print results
+      results
       end
 
     else
@@ -43,7 +45,7 @@ class GoogleNews
 
   end
 
-  def call_api
+  def call_api(formatted_search, num_of_articles, domain)
     suppress(Exception) do
       JSON.parse(open("https://www.googleapis.com/customsearch/v1?q=#{formatted_search}&cx=007438961960256472316%3A4gdkpqmpbru&num=#{num_of_articles}&siteSearch=#{domain}&sort=date&key=#{KEY}").read)
     end
